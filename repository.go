87345bcd50ce6b2a31b4a0dcef3010061e888e23
leonardoandrade <leonardo@andrade.io>
2015-09-17 08:19:18 +0200

    file metadata

--- repository.go
+++ repository.go
@@ -1,37 +1,43 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"math/rand"
 	"mime/multipart"
-	"io"
 	"os"
 )
 
 var hex_chars = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E"}
 
 type RepositoryStatus struct {
-	Directory      string   `json:"directory"`
+	Directory string `json:"directory"`
 	Config    Config `json:"config"`
 	CacheSize int    `json:"cache_size"`
 }
 
 type Repository struct {
 	directory string
-	config Config
-	cache  map[string]bool //simplistic cache
+	config    Config
+	cache     map[string]bool //simplistic cache
 }
 
+type FileMetadata struct {
+	OriginalName string  `json:"original_name"`
+	Location     *[2]int `json:"location"`
+}
 
-func MakeRepository(directory string) (Repository) {
-	cache := make(map[string]bool)
+func MakeRepository(directory string) Repository {
 	config, err := MakeConfigFromFile(directory + "/config.json")
 	if err != nil {
 		panic(err)
 	}
+	cache := make(map[string]bool)
 	return Repository{directory, config, cache}
 }
 
-func (r * Repository) makeGuid() (string){
+func (r *Repository) makeGuid() string {
 	var ret []byte = make([]byte, 32)
 	for i := 0; i < 32; i++ {
 		ret[i] = hex_chars[rand.Intn(len(hex_chars)-1)][0]
@@ -39,7 +45,7 @@ func (r * Repository) makeGuid() (string){
 	return string(ret[:])
 }
 
-func (r * Repository) Status() (RepositoryStatus) {
+func (r *Repository) Status() RepositoryStatus {
 	return RepositoryStatus{r.directory, r.config, len(r.cache)}
 }
 
@@ -53,19 +59,26 @@ func check_directory_emptyness(directory string) error {
 	return nil
 }
 
-
-func (r * Repository) saveImage(filename string, content *multipart.File) (string, error) {
+func (r *Repository) saveImage(filename string, content *multipart.File) (string, error) {
 	randName := r.makeGuid()
 
-
 	defer (*content).Close()
-
-	imageRoot := r.directory + "/" + string(randName[0]) + "/" + string(randName[1]) + "/"+ randName
+	imageRoot := r.directory + "/" + string(randName[0]) + "/" + string(randName[1]) + "/" + randName
 	os.MkdirAll(imageRoot, 0755)
 	f, _ := os.Create(imageRoot + "/original.png")
 	defer f.Close()
-
 	io.Copy(f, (*content))
+
+	metadata := FileMetadata{filename, nil}
+	jsonBytes, err := json.MarshalIndent(metadata, "", "\t")
+	if err != nil {
+		return "", err
+	}
+
+	err = ioutil.WriteFile(imageRoot+"/meta.json", jsonBytes, 0755)
+	if err != nil {
+		return "", err
+	}
 
 	return randName, nil
 }
